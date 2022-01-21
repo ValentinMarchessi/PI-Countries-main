9f6f25c69124a42b0b5fbab264b23185849eda00
Valentín Marchessi <vmarchessi@gmail.com>
2022-01-21 20:10:23 -0300

    logs for debugging

--- api/src/db.js
+++ api/src/db.js
@@ -6,6 +6,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
+console.log('Connecting sequelize to postgres...');
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
@@ -15,13 +16,16 @@ const basename = path.basename(__filename);
 const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
+console.log('Reading model definitions...');
 fs.readdirSync(path.join(__dirname, '/models'))
   .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
   .forEach((file) => {
     modelDefiners.push(require(path.join(__dirname, '/models', file)));
   });
 
+
 // Injectamos la conexion (sequelize) a todos los modelos
+console.log('Injecting sequelize into models...');
 modelDefiners.forEach(model => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
@@ -30,10 +34,17 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Countries } = sequelize.models;
-
+console.log('Setting up relations...');
+const { Country, Region, Subregion } = sequelize.models;
 // Aca vendrian las relaciones
-// Product.hasMany(Reviews);
+Region.hasMany(Country);
+Country.belongsTo(Region, {as: 'region'});
+
+Region.hasMany(Subregion);
+Subregion.belongsTo(Region, {as: 'region'});
+
+Subregion.hasMany(Country);
+Country.belongsTo(Subregion, {as: 'subregion'});
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
