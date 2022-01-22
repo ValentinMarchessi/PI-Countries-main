df93ddb218ee7043abc25ced1852dee9196b0fe1
Valentín Marchessi <vmarchessi@gmail.com>
2022-01-22 18:55:36 -0300

    added queries

--- api/src/routes/countries.js
+++ api/src/routes/countries.js
@@ -1,28 +1,13 @@
 const router = require('express').Router();
 const axios = require('axios');
 const { Op } = require('sequelize');
-const { Country, Activity } = require('../db.js')
+const { Country, Activity, conn } = require('../db.js')
 
 router.get('/', async function (req, res) {
-    const { name } = req.query
-    if (name) {
-        try {
-            const query_result = await Country.findAll({
-                where: {
-                    name: {
-                        [Op.iLike]: `${name}%`,
-                    },
-                }
-            });
-            query_result.length ?
-                res.json(query_result) :
-                res.status(400).json({ error: 'Could not find countries for the given query.' });
-        } catch (err) {
-            console.error(err);
-            res.status(400).send(err);
-        }
-    }
-    else try {
+    const { name, page } = req.query;
+
+    if (!await Country.count()) {
+        console.log('Countries table is empty, fetching countries from external API.');
         const { status, data } = await axios('https://restcountries.com/v3/all');
         data.forEach(function (country) {
             const { cca3, name, flags, region, capital } = country;
@@ -34,8 +19,42 @@ router.get('/', async function (req, res) {
                 capital: capital ? capital[0] : 'unavailable', 
             });
         })
-        const country_array = data.map((country) => country.name.common);
-        status === 200 ? res.status(200).json(country_array) : res.sendStatus(status);
+        console.log('Syncing countries table...');
+        await Country.sync({ alter: true });
+    }
+
+    if (Object.keys(req.query).length) {
+        if (name) {
+            try {
+                console.log(await Country.count());
+                const query_result = await Country.findAll({
+                    where: {
+                        name: {
+                            [Op.iLike]: `${name}%`,
+                        },
+                    }
+                }).catch(err => console.error(err));
+                query_result.length ?
+                    res.json(query_result) :
+                    res.status(400).json({ error: 'Could not find countries for the given query.' });
+            } catch (err) {
+                console.error(err);
+                res.status(400).send(err);
+            }
+        }
+        if (page) {
+            const { count, rows } = await Country.findAndCountAll({
+                offset: 10 * (page - 1),
+                limit: 10,
+            }).catch(function (err) {console.log(err)});
+            count ? res.status(200).send(rows) : res.sendStatus(400);
+        }
+    }
+    else try { 
+        country_array = await Country.findAll({
+            attributes: ['name']
+        })
+        country_array.length ? res.status(200).json(country_array) : res.sendStatus(400);
     } catch (error) {
         console.error(error);
         res.sendStatus(400);
@@ -72,6 +91,15 @@ router.get('/:countryId', async (req, res) => {
         console.error(error);
         res.sendStatus(400);
     }
+})
+
+router.get('/page/:num', async function (req, res) {
+    const { num } = req.params
+    const { count, rows } = await Country.findAndCountAll({
+        offset: 10 * (num - 1),
+        limit: 10,
+    }).catch(function (err) {console.log(err)});
+    count ? res.status(200).send(rows) : res.sendStatus(400);
 })
 
 module.exports = router;
