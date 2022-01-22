2c243b17cd7e2e642ce19fafb35546ba2547ac64
Valentín Marchessi <vmarchessi@gmail.com>
2022-01-22 18:56:39 -0300

    reformated

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,19 +1,11 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { fetchCountries } from '../../redux/actions';
-
+import CardContainer from '../CardContainer/CardContainer.jsx';
 import style from './Home.module.css';
 
 export default function Home() {
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch({ name: 'HOME_LOAD' });
-    },[dispatch])
 
     return (
         <div className={style.Home}>
-            Home
+            <CardContainer></CardContainer>
         </div>
     )
 }
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import style from './Navbar.module.css';
 
 export default function Navbar() {
+    const [input, setInput] = useState({
+        searchBar: ''
+    })
+
+    function handleChange(e){
+        const { value } = e.target;
+        setInput({ searchBar: value });
+    }
+
     return (
         <nav className={style.navbar}>
-           <button>Test</button>
         </nav>
     )
 }
