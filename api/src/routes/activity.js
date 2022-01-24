f06384749326bc31d516f7032669790dce583fcd
Valentín Marchessi <vmarchessi@gmail.com>
2022-01-24 05:36:36 -0300

    added activity POST and GET

--- api/src/routes/activity.js
+++ api/src/routes/activity.js
@@ -1,15 +1,57 @@
 const router = require('express').Router();
-const { Activity } = require('../db.js')
+const { Op } = require('sequelize');
+const { Activity, Country } = require('../db.js');
 
-router.post('/', function (req, res) {
-    const { name, difficulty, duration, season } = req.body;
-    if (!Object.keys(req.body).length) res.status(400).json({error: 'Invalid Input'});
-    else Activity.create({ name, difficulty, duration, season })
-        .then((activity) => res.status(201).json(activity))
-        .catch((err) => {
-            console.error(err);
-            res.status(400).send(err);
-        });
-})
+router.post('/', async function (req, res) {
+	const { name, difficulty, duration, season, countries } = req.body;
+	try {
+		if (!Object.keys(req.body).length) res.status(400).json({ error: 'Invalid Input' });
+		const activity = await Activity.create({ name, difficulty, duration, season });
+		console.log('Activity created successfully.');
+
+		let queries = countries.map((country) => {
+			return Country.findOne({
+				where: {
+					name: {
+						[Op.iLike]: country,
+					},
+				},
+			});
+		});
+		queries = await Promise.all(queries).then((results) => results).catch((error) => console.error(error));
+		queries.forEach((country) => {
+			country.addActivity(activity);
+		});
+
+		res.status(201).json(activity);
+	} catch (error) {
+		console.log(error);
+		res.sendStatus(400);
+	}
+});
+
+router.get('/', async (req, res) => {
+	const { country } = req.query;
+	if (country) {
+		try {
+			const results = await Activity.findAll({
+				include: [
+					{
+						model: Country,
+						through: {
+							where: { countryId: country },
+						},
+					},
+				],
+			}).catch(err => console.log(err));
+			results.length ? res.json(results) : res.status(404).send('No activities found.');
+			return;
+		} catch (err) {
+			console.log(err);
+			res.status(400).send(err);
+		}
+    }
+    res.status(400).send('Invalid Query');
+});
 
 module.exports = router;
