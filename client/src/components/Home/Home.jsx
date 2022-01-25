ca4e267c427f60a845f3d20d673cfd588d71d243
Valentín Marchessi <vmarchessi@gmail.com>
2022-01-25 01:44:48 -0300

    loads countries on home

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,15 +1,24 @@
 import CardContainer from '../CardContainer/CardContainer.jsx';
 import Searchbar from '../Searchbar/Searchbar.jsx';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { loadCountries } from '../../redux/actions';
 import style from './Home.module.css';
 
 export default function Home() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(loadCountries());
+    },[dispatch])
 
     return (
-        <div className={style.Home}>
-            <Searchbar />
-            <CardContainer />
-            <Link to= '/activity/create'>Create your activity!</Link>
-        </div>
-    )
+		<div className={style.Home}>
+			<h1 className={style.moto}>Find your destination...</h1>
+			<Searchbar />
+			<CardContainer />
+			<Link to="/activity/create">Create your activity!</Link>
+		</div>
+	);
 }
