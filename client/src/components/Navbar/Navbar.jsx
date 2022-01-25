c4460330cf759fc951f791f61211bc93768d68b0
Valentín Marchessi <vmarchessi@gmail.com>
2022-01-25 01:45:24 -0300

    added home and create activity link

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,11 @@
 import style from './Navbar.module.css';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
         <nav className={style.navbar}>
+            <Link to='/home'>Home</Link>
+            <Link to='/activity/create'>Create Activity</Link>
         </nav>
     )
 }
