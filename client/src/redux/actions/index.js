e886174f9d2aca417b28a4df2a3130c0e33020d4
Valentín Marchessi <vmarchessi@gmail.com>
2022-01-27 03:25:28 -0300

    added ordering parameters to get route

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,13 +1,13 @@
 const axios = require('axios');
 
-export function loadPages() {
+export function loadPages({orderBy, direction}) {
     const entries_per_page = 10;
     return async function (dispatch) {
         try {
             let pages = [];
             const max_index = await axios.get('http://localhost:3001/countries').then(({ data }) => data.length);
             for (let i = 1; i * entries_per_page <= max_index; i++) {
-                let { data } = await axios.get(`http://localhost:3001/countries?page=${i}`);
+                let { data } = await axios.get(`http://localhost:3001/countries?page=${i}&orderBy=${orderBy}&direction=${direction}`);
                 pages.push(data);
             }
             dispatch({type: 'LOAD_PAGES', payload: pages});     
