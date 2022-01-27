c3ef5bb4548c660ac339e65663bcfdf1addaa879
Valentín Marchessi <vmarchessi@gmail.com>
2022-01-27 03:24:37 -0300

    added ordering options

--- client/src/components/CardContainer/CardContainer.jsx
+++ client/src/components/CardContainer/CardContainer.jsx
@@ -7,16 +7,20 @@ import { loadPages } from '../../redux/actions';
 export default function CardContainer() {
     const [page, setPage] = useState([]);
     const [index, setIndex] = useState(1); /* page index */
+    const [options, setOptions] = useState({
+		orderBy: 'name', //name
+		direction: 'DESC',       //descending
+	});
 
     const dispatch = useDispatch();
     const pages = useSelector(store => store.pages);
 
     useEffect(() => {
-        dispatch(loadPages());
-    }, [dispatch])
+        dispatch(loadPages(options));
+    }, [dispatch, options])
     
     useEffect(() => {
-        if(pages[index]) setPage(pages[index - 1].map(country => 
+        if(pages[index - 1]) setPage(pages[index - 1].map(country => 
             <CountryCard
                 key={country.id}
                 id={country.id}
@@ -33,10 +37,39 @@ export default function CardContainer() {
         if (name === 'decrementPage' && index > 1) setIndex(index - 1);
     }
 
-    //console.log(page, pages);
+    function handleOptions(event){
+        event.preventDefault();
+        const { name } = event.target;
+        console.log(event);
+        switch (name) {
+            case "orderBy":
+                setOptions({ ...options, orderBy: options.orderBy === 'name' ? 'population' : 'name' });
+                break;
+            case "direction":
+                setOptions({ ...options, direction: options.direction === 'DESC' ? 'ASC' : 'DESC' });
+                break
+            default:
+                return new Error();
+        }
+    }
+
+    function handleFilter(event) {
+        
+    }
 
     return (
 		<div className={style.main}>
+			<div className={style.options}>
+				<button onClick={handleOptions} name="orderBy" className="material-icons">
+					{options.orderBy === 'name' ? 'sort_by_alpha' : 'group'}
+				</button>
+				<button onClick={handleOptions} name="direction" className="material-icons">
+					{options.direction === 'DESC' ? 'south' : 'north'}
+				</button>
+				<button onClick={handleFilter} name="filter" className="material-icons">
+                    filter_list
+				</button>
+			</div>
 			{page.length ? <div className={style.cardContainer}>{page}</div> : <div className="loader"></div>}
 			<div className={style.navigation}>
 				<button name="decrementPage" onClick={handlePageNav}>
