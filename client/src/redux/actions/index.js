aeedc0f3bbac2a1ca204d9ec927b3b4178c45dc0
Valentín Marchessi <vmarchessi@gmail.com>
2022-01-28 01:04:39 -0300

    added filter queries

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,14 +1,17 @@
 const axios = require('axios');
 
-export function loadPages({orderBy, direction}) {
+export function loadPages({order, filter}) {
     const entries_per_page = 10;
     return async function (dispatch) {
         try {
             let pages = [];
             const max_index = await axios.get('http://localhost:3001/countries').then(({ data }) => data.length);
+            const { type, value } = filter;
+            let query = `?orderBy=${order.by}&direction=${order.direction}`;
+            if (type && value) query += `&filter=${type}&filterValue=${value}`;
             for (let i = 1; i * entries_per_page <= max_index; i++) {
-                let { data } = await axios.get(`http://localhost:3001/countries?page=${i}&orderBy=${orderBy}&direction=${direction}`);
-                pages.push(data);
+                let { data } = await axios.get(`http://localhost:3001/countries${query}&page=${i}`);
+                if (data.length) pages.push(data);
             }
             dispatch({type: 'LOAD_PAGES', payload: pages});     
         }
@@ -35,6 +38,18 @@ export function loadActivities() {
         try {
             const activities = await axios.get(`http://localhost:3001/activity`).then(({ data }) => data);
             dispatch({ type: 'LOAD_ACTIVITIES', payload: activities });
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+}
+
+export function loadContinents() {
+    return async function (dispatch) {
+        try {
+            const continents = await axios.get('http://localhost:3001/countries/continents/').then(({ data }) => data);
+            dispatch({ type: 'LOAD_CONTINENTS', payload: continents });
         }
         catch (error) {
             console.error(error);
