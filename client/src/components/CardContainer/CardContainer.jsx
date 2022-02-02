b6930be5a53cd5be37134b5c237626742b8d8be7
Valentín Marchessi <vmarchessi@gmail.com>
2022-02-02 09:28:27 -0300

    moved page nav to independent component

--- client/src/components/CardContainer/CardContainer.jsx
+++ client/src/components/CardContainer/CardContainer.jsx
@@ -5,11 +5,39 @@ import { useDispatch, useSelector } from 'react-redux';
 import { loadPage } from '../../redux/actions';
 import SearchOptions from '../SearchOptions/SearchOptions.jsx';
 
-function PageNav({ index, handlePageNav }) {
-	const nextPage = useSelector(store => store.page.current.nextPage);
+function PageNav() {
+	const { count } = useSelector((store) => store.page.current);
+	const { filter } = useSelector((store) => store.page);
+	const [index, setIndex] = useState(1); /* page index */
+	const dispatch = useDispatch();
+	const pageCount = Math.ceil(count / 10)
+
+	useEffect(() => {
+		dispatch({ type: 'SET_INDEX', payload: index });
+	},[index,dispatch])
+
+	useEffect(() => {
+		setIndex(1); //por seguridad
+	}, [filter]);
+
+	function handlePageNav(event) {
+		const { name } = event.target;
+		if (name === 'decrementPage') setIndex(index - 1);
+		if (name === 'incrementPage') setIndex(index + 1);
+		if (name === 'gotoFirst') setIndex(1);
+		if (name === 'gotoLast') setIndex(pageCount);
+	}
+
 	return (
 		<div className={style.navigation}>
-			<div id="increment">
+			<div id="first">
+				{index > 1 ? (
+					<button className="material-icons" name="gotoFirst" title="Go to first page" onClick={handlePageNav}>
+						keyboard_double_arrow_left
+					</button>
+				) : null}
+			</div>
+			<div id="decrement">
 				{index > 1 ? (
 					<button className="material-icons" name="decrementPage" onClick={handlePageNav}>
 						navigate_before
@@ -17,10 +45,17 @@ function PageNav({ index, handlePageNav }) {
 				) : null}
 			</div>
 			<h1>{index}</h1>
-			<div id="decrement">
-				{nextPage ? (
+			<div id="increment">
+				{index < pageCount ? (
 					<button className="material-icons" name="incrementPage" onClick={handlePageNav}>
 						navigate_next
+					</button>
+				) : null}
+			</div>
+			<div id="last">
+				{index < pageCount ? (
+					<button className="material-icons" name="gotoLast" title="Go to last page" onClick={handlePageNav}>
+						keyboard_double_arrow_right
 					</button>
 				) : null}
 			</div>
@@ -29,25 +64,15 @@ function PageNav({ index, handlePageNav }) {
 }
 
 export default function CardContainer() {
-	const [index, setIndex] = useState(1); /* page index */
-
+	
 	const dispatch = useDispatch();
 	const { content } = useSelector((store) => store.page.current);
-	const {order, filter} = useSelector((store) => store.page);
+	const { order, filter } = useSelector((store) => store.page);
+	const index = useSelector(store => store.index)
 
 	useEffect(() => {
 		dispatch(loadPage(index,{ order, filter }));
 	}, [dispatch, order, filter, index]);
-
-	useEffect(() => {
-		setIndex(1); //por seguridad
-	},[filter])
-
-	function handlePageNav(event) {
-		const { name } = event.target;
-		if (name === 'incrementPage') setIndex(index + 1);
-		if (name === 'decrementPage') setIndex(index - 1);
-	}
 
 	return (
 		<div className={style.main}>
@@ -61,7 +86,7 @@ export default function CardContainer() {
 			) : (
 				<div className="loader"></div>
 			)}
-			<PageNav index={index} handlePageNav={handlePageNav} />
+			<PageNav/>
 		</div>
 	);
 }
