ed96e227ac72ec70cb5ad4265520e2a8f68bd65b
Valentín Marchessi <vmarchessi@gmail.com>
2022-02-04 13:54:02 -0300

    no changes

--- client/src/components/CountryCard/CountryCard.jsx
+++ client/src/components/CountryCard/CountryCard.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 export default function CountryCard({id, name, flag, continent}) {
     return (
-        <div className={style.card}>
-            <Link to={`/country/${id}`}>
-                <img src={flag} alt='flag'></img>
-            </Link>
-            <div className={style.infoBox}>
-                <h1>{name}</h1>
-                <span>{continent}</span>
-            </div>
-        </div>
-    )
+		<div className={style.card}>
+			<Link to={`/country/${id}`}>
+				<img src={flag} alt="flag"></img>
+			</Link>
+			<div className={style.infoBox}>
+				<h1>{name}</h1>
+				<span>{continent}</span>
+			</div>
+		</div>
+	);
 }
