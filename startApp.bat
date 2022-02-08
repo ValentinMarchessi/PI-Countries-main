95c1e912bb2d5c6e10475420323db18a27268935
Valentín Marchessi <vmarchessi@gmail.com>
2022-02-08 15:45:00 -0300

    back deploy

--- startApp.bat
+++
@@ -1,6 +0,0 @@
-cd C:\Users\usuario\Documents\Visual Studio 2019\Visual Code\Javascript\React\PI-Countries-main\api
-start Powershell -command "npm start"
-start code . 
-cd C:\Users\usuario\Documents\Visual Studio 2019\Visual Code\Javascript\React\PI-Countries-main\client
-start Powershell -command "npm start"
-start code .
